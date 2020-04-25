7fe9118466b6f6bfa7a56efd44e9e9baeb0d5dd4
RollingCompass <64215304+RollingCompass@users.noreply.github.com>
2020-04-25 09:36:37 +0200

    added atwood

--- models.py
+++ models.py
@@ -12,7 +12,8 @@ models = {
     'asimov': '1yg4bORU_KpV4h_aVnbMaekulK6ShpCS1',
     'hemingway': '1-0p2I9SCL37JEaIlIGhasbvOc4lxQIq6',
     'ursula-k-leguin': '1FRtM-kXn79COdVSautufne3dagViOcgM',
-    'garcia-marquez': '17S_FznIp1q-Px4D5sYIBi8RGcTsqkk7I'
+    'garcia-marquez': '17S_FznIp1q-Px4D5sYIBi8RGcTsqkk7I',
+    'atwood': '1JvjpKaIZVrmei-ujzY6hhB4NQmrWFqwV'
 }
 
 
